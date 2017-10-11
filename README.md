8cd8e9dfcd7bab62d0208cb4a9e3a47883c392cc
lllomh <lllomh@vip.qq.com>
2017-10-12 02:56:56 +0800

    shangc

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# vue2.0-mosuo
+# my-project
+
+> bolg form vue port  描述文件
+
+## Build Setup
+
+``` bash
+# 安装依赖
+npm install
+
+# 运行 at localhost:8080
+npm run dev
+
+# 发布打包
+npm run build
+```
+
+
+
+
+
+
+# 随手记
+
+
+
+``` js
+ new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+ })
+```
+ 
+``` js
+ 
+
+   template: '<App/>',
+    components: { App }
+      
+```
+
+### 用
+
+```js
+   render:x => x(App)
+```
+  
+###代替   2.0  
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+      <ul>
+          <li>
+              <router-link to="/Foo">Go to Foo</router-link>
+              <router-link to="/Bar">Go to Bar</router-link>
+          </li>
+      </ul>
+      <router-view></router-view>
+       <Hello></Hello>
+      <Hi></Hi>
+  </div>
+</template>
+
+<script>
+
+import Hello from './components/Hello.vue'
+import Hi from './components/Hi.vue'
+export default {
+  name: 'app',
+  components:{
+      Hello,
+      Hi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
